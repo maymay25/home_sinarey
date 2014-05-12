f670483df24ef5335845dddfa8e4a83815b8b8af
maymay25 <jeffrey6052@gmail.com>
2014-05-12 14:57:55 +0800

    unicorn fixed

--- config/initializers/establish_connection.rb
+++ config/initializers/establish_connection.rb
@@ -1,2 +1,7 @@
-ActiveRecord::Base.default_timezone = :local
-ActiveRecord::Base.establish_connection(Settings.web)
+
+def establish_connection!
+  ActiveRecord::Base.default_timezone = :local
+  ActiveRecord::Base.establish_connection(Settings.web)
+end
+
+establish_connection!
